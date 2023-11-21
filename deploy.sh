f2346ae33878e58a53b799836a55a01ffe5f67e1
Carlossawan <144855057+Carlossawan@users.noreply.github.com>
2023-11-22 00:24:16 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker pull carlossawan/gateway:latest
-docker stop gateway || true
-docker rm gateway || true
-docker run -d --name gateway -p 8080:8080 carlossawan/gateway:latest
+sudo docker pull carlossawan/gateway:latest
+sudo docker stop gateway || true
+sudo docker rm gateway || true
+sudo docker run -d --name gateway -p 8080:8080 carlossawan/gateway:latest
